b83624f7ee7504162b423cc3c59a34eac83aed49
Jose Luis <jgimenoperez@gmail.com>
2020-05-31 18:28:10 +0200

    Primera prueba auto

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const fs = require('fs');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello3 ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+
+var fecha = new Date();
+//var fso  = CreateObject("Scripting.FileSystemObject"); 
+//var FilePointer = FileOpener.OpenTextFile("hola-mundo.txt", 2, false);
+var texto =String
+
+texto="Día: "+fecha.getDate()+"/"+(fecha.getMonth()+1)+"/"+fecha.getFullYear()
+texto=texto + " Hora: "+fecha.getHours()+":"+fecha.getMinutes()+":"+fecha.getSeconds()+":"+fecha.getMilliseconds()
+console.log(texto);
+
+
+fs.writeFile("./holamundo.txt",texto, function (err) {
+  // la funcion es la que maneja lo que sucede despues de termine el evento
+  if (err) {
+      return console.log(err);
+  }
+  // las funciones de javascript en nodejs son asincronicas
+  // por lo tanto lo que se quiera hacer debe hacerse dentro de la funcion que maneja el evento
+  // si uno declara una variable arriba de la funcion, la manipula dentro y la quiere usar
+  // despues afuera, se corre el riezgo de que nunca se realice la manipulacion.
+  console.log("The file was saved!");
+});
